9c800a9f9a443105e928e291c6759e14f7c677bb
truonglx <truonglx@runsystem.net>
2024-01-24 16:05:21 +0700

    Change arrow function & update layout pokemon index.vue

--- src/views/Pokemon/index.vue
+++ src/views/Pokemon/index.vue
@@ -5,28 +5,28 @@ import { ref } from 'vue'
 const isShowIntro = ref(true)
 const mapSelect = ref(0)
 
-function clickSelectMap(map: number) {
+const clickSelectMap = (map: number) => {
   mapSelect.value = map
   isShowIntro.value = false
 }
+
+const mapGame = [
+  { id: 1, name: '2x2', row: 2, text: 'Very Easy' },
+  { id: 2, name: '4x4', row: 4, text: 'Easy' },
+  { id: 3, name: '6x6', row: 6, text: 'Normal' }
+]
 </script>
 
 <template>
   <div class="map-game" v-if="isShowIntro">
     <h1>POKE MEMORIES</h1>
     <p class="text">Select mode to start game</p>
-    <div class="map-options">
-      <div class="map-option" @click="clickSelectMap(2)">
-        <h2>2x2</h2>
-        <p>Very Easy</p>
-      </div>
-      <div class="map-option" @click="clickSelectMap(4)">
-        <h2>4x4</h2>
-        <p>Easy</p>
-      </div>
-      <div class="map-option" @click="clickSelectMap(6)">
-        <h2>6x6</h2>
-        <p>Normal</p>
+    <div class="map_list">
+      <div class="map-options" v-for="map in mapGame" :key="map.id">
+        <div class="map-option" @click="clickSelectMap(map.row)">
+          <h2>{{ map.name }}</h2>
+          <p>{{ map.text }}</p>
+        </div>
       </div>
     </div>
     <p class="footer">This game by <span>Xuan Truong</span></p>
@@ -36,4 +36,9 @@ function clickSelectMap(map: number) {
 
 <style scoped>
 @import './styles.scss';
+.map_list {
+  display: flex;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+}
 </style>
